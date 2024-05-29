9553d453ec675c683d5d17b8268c221b77b22885
Mark Carlton <mcarlton33@gmail.com>
2024-05-29 10:32:20 +0300

    create a gateway

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,13 +1,22 @@
-# Create a VPC
+# VPC
 resource "aws_vpc" "default-network" {
   cidr_block = var.cidr_block
 }
 
-# Create a subnet
+# Subnet
 resource "aws_subnet" "default_subnet" {
   vpc_id            = aws_vpc.default-network.id
   cidr_block        = var.cidr_block
   tags = {
     "name" = "${var.workspace}-subnet"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "default-gateway" {
+  vpc_id = aws_vpc.default-network.id
+
+  tags = {
+    Name = "${var.workspace}-net-gateway"
   }
 }
