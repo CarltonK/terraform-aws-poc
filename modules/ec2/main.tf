a624ce75774eee72a8a5f9f0d172ed978f105467
Mark Carlton <mcarlton33@gmail.com>
2024-05-29 11:21:49 +0300

    keep only vpc and subnet

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -12,57 +12,57 @@ resource "aws_subnet" "default_subnet" {
   }
 }
 
-# Internet Gateway
-resource "aws_internet_gateway" "default-gateway" {
-  vpc_id = aws_vpc.default-network.id
+# # Internet Gateway
+# resource "aws_internet_gateway" "default-gateway" {
+#   vpc_id = aws_vpc.default-network.id
 
-  tags = {
-    Name = "${var.workspace}-net-gateway"
-  }
-}
+#   tags = {
+#     Name = "${var.workspace}-net-gateway"
+#   }
+# }
 
-# Routing table
-resource "aws_route_table" "default-route" {
-  vpc_id = aws_vpc.default-network.id
+# # Routing table
+# resource "aws_route_table" "default-route" {
+#   vpc_id = aws_vpc.default-network.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.default-gateway.id
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.default-gateway.id
+#   }
 
-  tags = {
-    Name = "${var.workspace}-routing-table"
-  }
-}
+#   tags = {
+#     Name = "${var.workspace}-routing-table"
+#   }
+# }
 
-# Associate the route table with the subnet
-resource "aws_route_table_association" "default-route-subnet" {
-  subnet_id      = aws_subnet.default_subnet.id
-  route_table_id = aws_route_table.default-route.id
-}
+# # Associate the route table with the subnet
+# resource "aws_route_table_association" "default-route-subnet" {
+#   subnet_id      = aws_subnet.default_subnet.id
+#   route_table_id = aws_route_table.default-route.id
+# }
 
-# Security Group
-resource "aws_security_group" "default-security-group" {
-  vpc_id = aws_vpc.default-network.id
+# # Security Group
+# resource "aws_security_group" "default-security-group" {
+#   vpc_id = aws_vpc.default-network.id
 
-  # ingress {
-  #   from_port   = 22
-  #   to_port     = 22
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
+#   # ingress {
+#   #   from_port   = 22
+#   #   to_port     = 22
+#   #   protocol    = "tcp"
+#   #   cidr_blocks = ["0.0.0.0/0"]
+#   # }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    Name = "${var.workspace}-security-group"
-  }
-}
+#   tags = {
+#     Name = "${var.workspace}-security-group"
+#   }
+# }
 
 # # EC2 instance
 # resource "aws_instance" "default-instance" {
