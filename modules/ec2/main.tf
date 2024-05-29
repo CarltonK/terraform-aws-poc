056740b1922de05c6da811df6e00842d45af2651
Mark Carlton <mcarlton33@gmail.com>
2024-05-29 10:36:32 +0300

    creating route table to subnet

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -20,3 +20,23 @@ resource "aws_internet_gateway" "default-gateway" {
     Name = "${var.workspace}-net-gateway"
   }
 }
+
+# Routing table
+resource "aws_route_table" "default-route" {
+  vpc_id = aws_vpc.default-network.id
+
+  route {
+    cidr_block = var.cidr_block
+    gateway_id = aws_internet_gateway.default-gateway.id
+  }
+
+  tags = {
+    Name = "${var.workspace}-routing-table"
+  }
+}
+
+# Associate the route table with the subnet
+resource "aws_route_table_association" "default-route-subnet" {
+  subnet_id      = aws_subnet.default_subnet.id
+  route_table_id = aws_route_table.default-route.id
+}
