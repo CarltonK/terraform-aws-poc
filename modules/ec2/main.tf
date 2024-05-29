d0d8ae90f811f7ce8e93ec208f3569a8896937c2
Mark Carlton <mcarlton33@gmail.com>
2024-05-29 11:07:05 +0300

    skip ingress from port 22

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -45,12 +45,12 @@ resource "aws_route_table_association" "default-route-subnet" {
 resource "aws_security_group" "default-security-group" {
   vpc_id = aws_vpc.default-network.id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # ingress {
+  #   from_port   = 22
+  #   to_port     = 22
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
 
   egress {
     from_port   = 0
